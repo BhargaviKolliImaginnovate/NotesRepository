a628367221f4397b0e269cb97fb1907eb587be57
BhargaviKolliImaginnovate <129928586+BhargaviKolliImaginnovate@users.noreply.github.com>
2023-05-24 15:08:05 +0530

    Add files via upload

--- NoteRepository.java
+++ NoteRepository.java
@@ -2,7 +2,9 @@ package Notes.com.example.Notesdemo.repository;
 
 import Notes.com.example.Notesdemo.entity.Notes;
 import org.springframework.data.domain.ManagedTypes;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,8 +17,8 @@ public interface NoteRepository extends JpaRepository<Notes,Integer> {
 
    List<Notes> getByUserId(int userId);
 
-//   @Query("From notes where notes.id=:id")
-//   Notes findById(@Param("id") int id);
 
+
+   Page<Notes> findAll(Pageable pageable);
 
 }
